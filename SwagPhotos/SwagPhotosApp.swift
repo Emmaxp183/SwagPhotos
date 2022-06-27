198972ed02327e5fd174c01f76aacb54a981a6ab
Emmanuel Mensah <41510090+Emmaxp183@users.noreply.github.com>
2022-06-27 20:29:13 +0200

    Initial Commit

---
+++ SwagPhotos/SwagPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwagPhotosApp.swift
+//  SwagPhotos
+//
+//  Created by macbook on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwagPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
